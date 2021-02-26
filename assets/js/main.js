5f5d9357bb224ea8a5bafb6bbfe1652769f8c219
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-02-26 14:17:27 +0100

    completo

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+var nome = prompt ("Qual è il tuo nome?");
+console.log(nome);
+var cognome = prompt ("Qual è il tuo cognome?");
+console.log(cognome);
+document.getElementById('pw').innerHTML=nome+cognome+21
