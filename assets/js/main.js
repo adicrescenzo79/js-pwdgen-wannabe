e1944f43e65e253a7b97d386e67744ebaf8ddbc8
Alessandro Di Crescenzo <adicrescenzo79@gmail.com>
2021-02-26 15:01:14 +0100

    completo davvero

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,6 @@ var nome = prompt ("Qual è il tuo nome?");
 console.log(nome);
 var cognome = prompt ("Qual è il tuo cognome?");
 console.log(cognome);
-document.getElementById('pw').innerHTML=nome+cognome+21
+var colore = prompt ("Qual è il tuo colore preferito?");
+console.log(colore);
+document.getElementById('pw').innerHTML=nome+cognome+colore+21;
